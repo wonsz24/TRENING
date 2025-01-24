d8dde9ae160891576442a6c2855619950a080361
Marcin Zaporowski <m.zaporowski2411@gmail.com>
2025-01-24 23:52:50 +0100

    ustawienia

---
+++ TRENING/ProfilePhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePhotoView.swift
+//  TRENING
+//
+//  Created by Marcin Zaporowski on 24/01/2025.
+//
+
+import SwiftUI
+
+struct ProfilePhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePhotoView()
+}
