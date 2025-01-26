563a4c9692c270d91d0600bbef4bb15a89ef9cc5
wonsz24 <m.zaporowski2411@gmail.com>
2025-01-26 09:56:04 +0100

    EditProfileView

--- TRENING/EditProfileView.swift
+++ TRENING/EditProfileView.swift
@@ -1,18 +1,18 @@
 //
-//  ProfilePhotoView.swift
+//  EditProfileView.swift
 //  TRENING
 //
-//  Created by Marcin Zaporowski on 24/01/2025.
+//  Created by Marcin Zaporowski on 25/01/2025.
 //
 
 import SwiftUI
 
-struct ProfilePhotoView: View {
+struct EditProfileView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ProfilePhotoView()
+    EditProfileView()
 }
